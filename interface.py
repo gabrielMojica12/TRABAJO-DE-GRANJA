9421b6942d098dcab5110d2d6fd9ab524bbe47b7
gabrielMojica12 <videosgabriel4@gmail.com>
2025-11-05 23:54:44 -0500

    Add Interface class with CRUD methods

---
+++ interface.py
@@ -0,0 +1,12 @@
+class Interface:
+    def crear(self, animal):
+        pass
+
+    def leer(self, codigo):
+        pass
+
+    def actualizar(self, codigo, nuevos_datos):
+        pass
+
+    def eliminar(self, codigo):
+        pass
